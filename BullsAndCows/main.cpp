19ef83a35950ff18958b4889d32ce22664c09979
Faelwen <francis.john.mccarthy@googlemail.com>
2017-08-30 01:04:08 +0200

    Simplifying with functions

--- BullsAndCows/main.cpp
+++ BullsAndCows/main.cpp
@@ -3,29 +3,29 @@
 
 using namespace std;
 
+void PrintIntroduction();
+string GetGuess();
+
+
 int main()
 {
-	constexpr int WORD_LENGTH = 5;
+	PrintIntroduction();
+	GetGuess();
+	return 0;
+}
 
+void PrintIntroduction() {
 	// Introduce the game
+	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?" << endl;
+}
 
-	// Get a guess from the user
+string GetGuess() {
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-
-	// Repeat the guess back to the user
-	cout << "Your guess was: " << Guess << endl;
-
-	// Get a guess from the user
-	cout << "Enter your guess: ";
-	getline(cin, Guess);
-
-	// Repeat the guess back to the user
-	cout << "Your guess was: " << Guess << endl;
-
-	return 0;
+	cout << "Your guess was:" << Guess << endl;
+	return Guess;
 }
