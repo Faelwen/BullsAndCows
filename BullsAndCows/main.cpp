ebc7a02067bb3f92efedd0c15b95134c2e6fdfcb
Faelwen <francis.john.mccarthy@googlemail.com>
2017-08-30 00:38:44 +0200

    cin & variables

--- BullsAndCows/main.cpp
+++ BullsAndCows/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -6,9 +7,25 @@ int main()
 {
 	constexpr int WORD_LENGTH = 5;
 
+	// Introduce the game
 	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?" << endl;
+
+	// Get a guess from the user
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
+
+	// Repeat the guess back to the user
+	cout << "Your guess was: " << Guess << endl;
+
+	// Get a guess from the user
+	cout << "Enter your guess: ";
+	cin >> Guess;
+
+	// Repeat the guess back to the user
+	cout << "Your guess was: " << Guess << endl;
 
 	return 0;
 }
